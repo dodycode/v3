56356ace0dc0bee83de79d00e59a795e3af325a7
Dody <prasetyodody17@gmail.com>
2024-09-03 22:57:24 +0700

    🧱 Vercel setup
    feat: vercel setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,17 @@ import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   site: "https://dodycode.com",
+
   headers: {
     "Content-Security-Policy": `script-src 'self' 'unsafe-inline' https://giscus.app;`,
   },
+
+  output: "server",
+  adapter: vercel(),
 });
