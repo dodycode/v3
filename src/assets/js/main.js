fa069276c89c3a431582a705883a08c00db4d6fe
Dody <prasetyodody17@gmail.com>
2024-09-04 19:25:53 +0700

    🎨 Adjust the food size to fit in big oven
    adjust the food size to fit in big oven

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -10,6 +10,8 @@ const stickyClassesContainer = [
   "dark:border-neutral-600/40",
   "dark:bg-neutral-900/60",
   "backdrop-blur-2xl",
+  "2xl:pl-6",
+  "2xl:pr-4",
 ];
 const unstickyClassesContainer = ["border-transparent"];
 let headerElement = null;
